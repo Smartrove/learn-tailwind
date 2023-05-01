2011257b34fafe82e016e6112d3b8772cd9dfcd8
smartrove <olaokunolalekan@gmail.com>
2023-05-01 23:24:25 +0100

    updating

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import Landingpage from "./components/Landingpage";
+import React from "react";
+import LandingPage from "./components/LandingPage";
 
 function App() {
   return (
-    <div className="App">
-      <Landingpage />
+    <div>
+      <LandingPage />
     </div>
   );
 }
--- src/components/Landingpage.jsx
+++ src/components/Landingpage.jsx
@@ -1,30 +1,32 @@
 import React from "react";
 import imageOne from "../assets/stew.jpg";
-
+// import "../input.css";
 const LandingPage = () => {
   return (
     <div>
       <div>
         <nav>
           <div>
-            <h1>
-              <a href="/">Order App</a>
+            <h1 className="font-bold uppercase">
+              <a href="/" className="text-gray-600">
+                Order App
+              </a>
             </h1>
           </div>
           <ul>
-            <li>
+            <li className="font-bold">
               <a href="#">
-                <span>Home</span>
+                <span className="text-gray-600">Home</span>
               </a>
             </li>
             <li>
               <a href="#about">
-                <span>About</span>
+                <span className="text-gray-600">About</span>
               </a>
             </li>
             <li>
               <a href="#contact">
-                <span>Contact</span>
+                <span className="text-gray-600">Contact</span>
               </a>
             </li>
           </ul>
@@ -32,32 +34,41 @@ const LandingPage = () => {
       </div>
       <main>
         <div>
-          <a href="#login" Login></a>
-          <a href="#signup" Sign up></a>
+          <a href="#login" className="text-gray-600">
+            Login
+          </a>
+          <a href="#signup" className="text-gray-600">
+            Sign up
+          </a>
         </div>
 
         <header>
-          <h2>Recipes</h2>
-          <h2>For Order</h2>
+          <h2 className="text-gray-600 text-6xl font-semibold">Recipes</h2>
+          <h2
+            className="text-gray-600 text-2xl font-semibold
+          "
+          >
+            For Order
+          </h2>
         </header>
 
         <div>
-          <h4>Latest Recipes</h4>
+          <h4 className="text-gray-600 font-bold">Latest Recipes</h4>
 
           <div>
             <img src={imageOne} alt="" />
 
             <div>
-              <span>5 Bean Chilli Stew</span>
-              <span>Recipe by special</span>
+              <span className="text-gray-600">5 Bean Chilli Stew</span>
+              <span className="text-gray-600">Recipe by special</span>
             </div>
           </div>
 
-          <h4>Most Popular</h4>
+          <h4 className="text-gray-600 font-bold">Most Popular</h4>
           <div>{/* card goes here  */}</div>
 
           <div>
-            <div>Load More</div>
+            <div className="text-gray-600">Load More</div>
           </div>
         </div>
       </main>
